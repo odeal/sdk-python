51d9ed277780df449c31cf815e6af397677de9c7
Murat Yılmaz <murat.yilmaz@ode.al>
2026-06-26 08:56:37 +0000

    Release SDK version 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odeal-sdk',
-    version='0.1.4',
+    version='0.1.5',
     description='Odeal Entegrasyon SDK (Otomatik Üretildi)',
     author='Odeal',
     author_email='',
--- src/odeal_sdk/__init__.py
+++ src/odeal_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 from .odeal_config import OdealConfig
 from .odeal_config_builder import OdealConfigBuilder
 from .odeal_environment import OdealEnvironment
