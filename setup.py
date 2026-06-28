901573af5227541f20c84344a02f6663a8c515b6
Murat Yılmaz <murat.yilmaz@ode.al>
2026-06-28 13:06:53 +0000

    Release SDK version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odeal-sdk',
-    version='0.1.5',
+    version='0.1.6',
     description='Odeal Entegrasyon SDK (Otomatik Üretildi)',
     author='Odeal',
     author_email='',
--- src/odeal_sdk/base_resource.py
+++ src/odeal_sdk/base_resource.py
@@ -39,7 +39,7 @@ class BaseResource:
         config: SDK yapılandırma ayarları.
     """
     
-    AGENT = "OdealSdkPythonClient/0.1.5"
+    AGENT = "OdealSdkPythonClient/0.1.6"
     
     def __init__(self, config: OdealConfig):
         """
